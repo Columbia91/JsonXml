a9da138cef79b1b8175aee180fec859955000403
ADILET <alpha.z91@mail.ru>
2019-02-18 00:46:45 +0600

    Добавьте файлы проекта.

---
+++ JsonXmlHomework/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace JsonXmlHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("Qwerty", "1234As!", "developer@gmail.com", "+77021021213");
+            User user2 = new User("Maestro", "#Piano55", "musicman80@mail.com", "+77782880808");
+            User user3 = new User("mr.Smith", "Coollife88$", "smittytime@gmail.com", "+77071072727");
+
+            User[] users = new User[] { user1, user2, user3 };
+
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(User[]));
+
+            using (FileStream fs = new FileStream(@"C:\\people.txt", FileMode.OpenOrCreate))
+            {
+                jsonFormatter.WriteObject(fs, users);
+            }
+        }
+    }
+}
---
+++ JsonXmlHomework/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonXmlHomework
+{
+    class User
+    {
+        private string Login { get; set; }
+        private string Password { get; set; }
+        private string Email { get; set; }
+        private string PhoneNumber { get; set; }
+
+        public User(string login, string password, string email, string phoneNumber)
+        {
+            Login = login; Password = password;
+            Email = email; PhoneNumber = phoneNumber;
+        }
+    }
+}
