9da7862297b23dd67d20e3ea45226ccf3b67d830
ADILET <alpha.z91@mail.ru>
2019-02-19 16:58:14 +0600

    json and xml

--- JsonXmlHomework/User.cs
+++ JsonXmlHomework/User.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JsonXmlHomework
 {
     [Serializable]
     public class User
     {
+        public string Email { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
-        public string Email { get; set; }
         public string PhoneNumber { get; set; }
 
         public User() {}
